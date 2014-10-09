0bd78d15b67960d48427de5219311f7c59063d76
Sarah Batchu <sarahbatchu@gmail.com>
2014-10-08 20:19:49 -0400

    Created toggle button

--- chapter_02/01_document_ready/script.js
+++ chapter_02/01_document_ready/script.js
@@ -1,12 +1,12 @@
 $(document).ready(function(){
   $("#celebs tbody tr:even").addClass("zebra");
-  
-  $('#hideButton').click(function(){
-  	$('#disclaimer').hide();
+  $('#toggleButton').click(function (){
+  		if($('#disclaimer').is(':visible')){
+  			$('#disclaimer').hide();
+  		}
+  		else {
+  			$('#disclaimer').show
+  		}
   })
-  $('#showButton').click(function(){
-  	$('#disclaimer').show();
-  })
-
 
 });
