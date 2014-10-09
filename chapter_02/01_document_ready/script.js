b97742b115bd75dc6ee735e00e0c703504bcba40
Sarah Batchu <sarahbatchu@gmail.com>
2014-10-08 20:04:11 -0400

    added show button and made "hide" default

--- chapter_02/01_document_ready/script.js
+++ chapter_02/01_document_ready/script.js
@@ -4,7 +4,9 @@ $(document).ready(function(){
   $('#hideButton').click(function(){
   	$('#disclaimer').hide();
   })
-
+  $('#showButton').click(function(){
+  	$('#disclaimer').show();
+  })
 
 
 });
